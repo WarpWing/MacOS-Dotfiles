c740cab2938912512c3b9e86012b1bd2c855bd97
Ty Chermsirivatana <professornitro@gmail.com>
2020-07-22 08:57:39 -0400

    Reset script

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@ echo "Please type the commit message: "
 read INPUT   
 git add .
 git commit -m "$INPUT"
-git push -f origin master 
+git push -u origin master 
 reset
 echo "Git commit has been pushed and commited to the remote branch"
